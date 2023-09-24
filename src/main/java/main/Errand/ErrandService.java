abb7f89f3e8943a11ddbacf05f3cac2cc8a5dfc2
singleaspringle <bartek.staszynski@gmail.com>
2023-09-24 18:52:35 +0200

    Dodana wyszukiwarka dla Car. Dodane response message w kazdym apiResponsie, walidacja przy tworzeniu cara.
    (cherry picked from commit 0e7ab98cf25da960bed55e4ad57f6d11f0a087f9)

--- src/main/java/main/Errand/ErrandService.java
+++ src/main/java/main/Errand/ErrandService.java
@@ -4,14 +4,23 @@ import main.Car.Car;
 import main.Car.CarService;
 import main.Driver.Driver;
 import main.Driver.DriverService;
+import main.ErrandData.ErrandData;
+import main.ErrandData.ErrandDataService;
+import main.ErrandData.ErrandStatus;
 import main.Location.Location;
 import main.Location.LocationService;
 import org.h2.util.json.JSONArray;
 import org.h2.util.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
+
+import static main.ErrandData.ErrandDataService.getCompletedPointsByErrandId;
+import static main.Location.LocationService.getListOfRealAddresses;
 
 @Service
 public class ErrandService {
@@ -22,7 +31,12 @@ public class ErrandService {
     }
 
     public void addNewErrand(Errand errand){
-        repository.save(errand);
+        if(!isDrvIdValid(errand.getDrvId())) throw new ResponseStatusException(HttpStatus.CONFLICT, "Driver with that ID does not exist");
+        if(!isCarIdValid(errand.getCarId())) throw new ResponseStatusException(HttpStatus.CONFLICT, "Car with that ID does not exist");
+        if(isDrvIdValid(errand.getDrvId()) && isCarIdValid(errand.getCarId())){
+            repository.save(errand);
+            ErrandDataService.generateNewDataRecord(errand);
+        }
     }
 
     List<Errand> getByDrvId(Long drvId){
@@ -53,16 +67,23 @@ public class ErrandService {
         return checkedDriver.isPresent();
     }
 
+    private Boolean isCarIdValid(Long carId){
+        Optional<Car> checkedCar = CarService.getCarById(carId);
+        return checkedCar.isPresent();
+    }
+
     @Transactional
     public void editErrand(Long errandId, Long carId, Long drvId, String newRoute){
         Errand manipulatedErrand = getByErrandId(errandId).orElseThrow(
-                () -> new IllegalStateException("Errand with that id does not exist")
+                () -> new ResponseStatusException(HttpStatus.CONFLICT, "Errand with that id does not exist")
         );
-        if(drvId != null && isDrvIdValid(drvId)){
-            manipulatedErrand.setDrvId(drvId);  //dac exception jesli jest invalid
+        if(drvId != null){
+            if(isDrvIdValid(drvId)) manipulatedErrand.setDrvId(drvId);
+            else throw new ResponseStatusException(HttpStatus.CONFLICT, "Driver ID is invalid");
         }
-        if(newRoute != null && isRouteValid(newRoute)) {
-            manipulatedErrand.setPlannedRouteAsString(newRoute);    //dac exception jesli jest invalid
+        if(newRoute != null) {
+            if(isRouteValid(newRoute))  manipulatedErrand.setPlannedRouteAsString(newRoute);
+            else throw new ResponseStatusException(HttpStatus.CONFLICT, "Route is invalid");
         }
     }
 
@@ -70,9 +91,10 @@ public class ErrandService {
         return repository.findAll();
     }
 
-    public List<Errand> searchErrands(String firstNamePart, String lastNamePart, String makePart, String modelPart, Integer batchNumber){
+    public ResponseEntity<Object> searchErrands(String firstNamePart, String lastNamePart, String makePart, String modelPart, Integer batchNumber){
         List<Errand> allErrands = repository.findAll();
-        List<Errand> matchedErrands = new ArrayList<>();
+        List<Errand> matchedErrands = new ArrayList<Errand>();
+        List<Errand> responseErrandList = new ArrayList<Errand>();
 
         if(firstNamePart == null)   firstNamePart = "";
         if(lastNamePart == null)    lastNamePart = "";
@@ -80,34 +102,57 @@ public class ErrandService {
         if(modelPart == null)   modelPart = "";
 
         for(Errand errand : allErrands){
-            Car car = CarService.getCarById(errand.getCarId()).get();
-            Driver driver = DriverService.getDriverById(errand.getDrvId()).get();
+            Car car = CarService.getCarById(errand.getCarId()).orElse(null);
+            Driver driver = DriverService.getDriverById(errand.getDrvId()).orElse(null);
 
-            Boolean errandMatchesSearch = false;
-            errandMatchesSearch = driver.getFirstName().contains(firstNamePart) && driver.getLastName().contains(lastNamePart) &&
-                    car.getMake().contains(makePart) && car.getModel().contains(modelPart);
+            if(car != null && driver != null) {
+                Boolean errandMatchesSearch = false;
+                errandMatchesSearch = driver.getFirstName().contains(firstNamePart) && driver.getLastName().contains(lastNamePart) &&
+                        car.getMake().contains(makePart) && car.getModel().contains(modelPart);
 
-            if (errandMatchesSearch)    matchedErrands.add(errand);
+                if (errandMatchesSearch) matchedErrands.add(errand);
+            }
         }
 
-        if(matchedErrands.isEmpty())    throw new IllegalStateException("no results found");
+        if(matchedErrands.isEmpty())    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No results found");
+        else {
+            Integer startIndex = batchNumber * 10 - 10;
+            Integer endIndex = batchNumber * 10;
+            if (matchedErrands.size() > startIndex + endIndex && matchedErrands.size() > startIndex) {
+                responseErrandList = matchedErrands.subList(startIndex, endIndex);
+            } else if (matchedErrands.size() > startIndex) {
+                responseErrandList = matchedErrands.subList(startIndex, matchedErrands.size());
+            } else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Batch is empty");
+            }
+        }
 
-        Integer startIndex = batchNumber*15 - 15;
-        Integer endIndex = batchNumber*15;
-        if(matchedErrands.size() > startIndex + endIndex && matchedErrands.size() > startIndex){
-            return matchedErrands.subList(startIndex, endIndex);
-        } else if (matchedErrands.size() > startIndex) {
-            return matchedErrands.subList(startIndex, matchedErrands.size());
-        } else {
-            throw new IllegalStateException("batch is empty");
+        Integer numberOfBatches = (Integer) (matchedErrands.size()/10) + 1;
+
+        Map<String, Object> response = new HashMap<String, Object>();
+        List<Object> listOfErrands = new ArrayList<Object>();
+        try{
+            for(Errand errand : responseErrandList){
+                Map<String, Object> singleErrandFields = new HashMap<String, Object>();
+                singleErrandFields.put("errand", errand);
+                singleErrandFields.put("realAddressList", getListOfRealAddresses(errand.getPlannedRouteAsList()));
+                singleErrandFields.put("completedPoints", getCompletedPointsByErrandId(errand.getErrandId()));
+                listOfErrands.add(singleErrandFields);
+            }
+            response.put("errands", listOfErrands);
+            response.put("size", numberOfBatches);
+            return new ResponseEntity<Object>(response, HttpStatus.OK);
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error");
         }
     }
 
-//    public JSONArray displayMatchedErrands(String firstNamePart, String lastNamePart, String makePart, String modelPart, Integer batchNumber){
-//        List<Errand> matchedErrands = this.searchErrands(firstNamePart, lastNamePart, makePart, modelPart, batchNumber);
-//        for(Errand errand : matchedErrands){
-//            JSONObject errandJson = errand.createJsonObject();
-//        }
-//    }
-    //dodac sklejanie, tak zeby do kazdego wyszukanego erranda dodac liste jego realAdressow i completed points
+    public void deleteErrandById(Long errandId) {
+        if (!getByErrandId(errandId).isPresent()) throw new IllegalStateException("Errand with that id does not exist");
+        else {
+            repository.deleteById(errandId);
+            //ErrandDataService.deleteDataById(errandId);   //usuwanie jest niepotrzebne. Jak usuwa sie obiekt klasy Errand, to ErrandData od razu jest usuwane
+        }
+    }
 }
